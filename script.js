a3854bf9c85f260e6f6da96fb1907f67f47f8f9d
Felipe Toro <felipetoro0832@gmail.com>
2020-12-11 01:26:57 -0500

    Creacion de caras de las cartas

--- script.js
+++ script.js
@@ -3,22 +3,22 @@ const btn18 = document.getElementById('btn18');
 const btn24 = document.getElementById('btn24');
 const btn30 = document.getElementById('btn30');
 const imgs = [
-  '<img src="./imgs/magic.png" alt="Magia"></img>',
-  '<img src="./imgs/bat.png" alt="Murcielago">',
-  '<img src="./imgs/bear.png" alt="Oso">',
-  '<img src="./imgs/cat.png" alt="Gato">',
-  '<img src="./imgs/dolphin.png" alt="Delfin">',
-  '<img src="./imgs/elephant.png" alt="Elefante">',
-  '<img src="./imgs/flamingo.png" alt="Flamenco">',
-  '<img src="./imgs/frog.png" alt="Rana">',
-  '<img src="./imgs/giraffe.png" alt="Jirafa">',
-  '<img src="./imgs/koala.png" alt="Koala">',
-  '<img src="./imgs/owl.png" alt="Buho">',
-  '<img src="./imgs/penguin.png" alt="Pinguino">',
-  '<img src="./imgs/pig.png" alt="Cerdo">',
-  '<img src="./imgs/snake.png" alt="Serpiente">',
-  '<img src="./imgs/wolf.png" alt="Lovo">',
-  '<img src="./imgs/bee.png" alt="Aveja"></img>'  
+  "./imgs/magic.png",
+  "./imgs/bat.png",
+  "./imgs/bear.png",
+  "./imgs/cat.png",
+  "./imgs/dolphin.png",
+  "./imgs/elephant.png",
+  "./imgs/flamingo.png",
+  "./imgs/frog.png",
+  "./imgs/giraffe.png",
+  "./imgs/koala.png",
+  "./imgs/owl.png",
+  "./imgs/penguin.png",
+  "./imgs/pig.png",
+  "./imgs/snake.png",
+  "./imgs/wolf.png",
+  "./imgs/bee.png"  
 ]
 
 function $(selector) {
@@ -29,8 +29,9 @@ class Game {
   constructor (gameSize) {
     this.imgs = imgs
     this.array_imgs = []
-    this.createGame (gameSize)
     this.generate_array_imgs (gameSize)
+    this.disorganize_array_imgs ()
+    this.createGame (gameSize)
     console.log (this.imgs)
     console.log (this.array_imgs)
   }
@@ -45,11 +46,18 @@ class Game {
       $('#game').appendChild(carta_container);
 
       for (let j = 0; j <= 1; j++) {
+        // debugger
         let carta = document.createElement('span');
-        carta.setAttribute ('id', 'carta' + j);
+        let icon = document.createElement('img');
         carta.setAttribute ('class', 'carta');
+        if (j === 0) {
+          icon.src = this.imgs[0]
+        } else {
+          icon.src = this.array_imgs[i]
+          console.log (this.array_imgs[i])
+        }
         $('#carta_container' + i).appendChild(carta);
-        // console.log ('loop inside')
+        carta.appendChild(icon)
       }
     }  
   }
@@ -65,6 +73,17 @@ class Game {
       this.imgs.splice(random_imgs, 1)
     } 
   }
+
+  disorganize_array_imgs () {
+    this.array_imgs.sort(() => Math.random() - 0.5)
+    console.log(this.array_imgs)
+  }
+
+  // assign_cards () {
+  //   for (let i = 0; i < ((gameSize * GAME_ROWS) / 2); i++) {
+  //     let carta = document.getElementById('carta0')
+  //   }
+  // }
 
 
 }
