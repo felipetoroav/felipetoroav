5b6da255c20eb36c6c9b50c7283d922634b15d2a
Felipe Toro <felipetoro0832@gmail.com>
2020-12-20 14:47:28 -0500

    finalizado juego y puntajes

--- script.js
+++ script.js
@@ -3,6 +3,9 @@ const btn18 = document.getElementById('btn18');
 const btn24 = document.getElementById('btn24');
 const btn30 = document.getElementById('btn30');
 const menu = document.getElementsByClassName('game-config');
+const aciertos = document.getElementById('aciertos_seguidos');
+const errores = document.getElementById('errores');
+const puntaje = document.getElementById('puntaje');
 const imgs = [
   "./imgs/magic.png",
   "./imgs/bat.png",
@@ -33,6 +36,10 @@ class Game {
     this.second_cont
     this.gameSize = gameSize
     this.selected_cards = 0
+    this.acierto_seguido = 0
+    this.aciertos_seguidos = 0
+    this.errores = 0
+    this.puntaje = 0
     this.imgs = imgs
     this.array_imgs = []
     this.createGame ()
@@ -42,7 +49,6 @@ class Game {
     this.generateArrayImgs ()
     this.hideMenu ()
     this.chooseCard = this.chooseCard.bind(this)
-    // console.log(this.first_card + 'createGame function')
     for (let i = 0; i < (GAME_ROWS * this.gameSize); i++){
       let card_container = document.createElement('div');
       card_container.setAttribute ('id', 'card_container' + i)
@@ -50,6 +56,7 @@ class Game {
       card_container.setAttribute ('data-container', i)
       card_container.setAttribute ('class', 'card_container')
       $('#game').appendChild(card_container)
+      this.addClickEvents (i)
 
       for (let j = 0; j <= 1; j++) {
         let card = document.createElement('span')
@@ -69,11 +76,9 @@ class Game {
         $('#card_container' + i).appendChild(card)
       }
     }
-    this.addClickEvents ()
   }
 
   generateArrayImgs () {
-    // console.log(this.first_card + 'generateArrayImgs function')
     let random_imgs, face;
     for (let i = 1; i <= ((this.gameSize * GAME_ROWS) / 2); i++) {
       random_imgs =  Math.floor(Math.random() * (imgs.length - 1) + 1)
@@ -90,41 +95,62 @@ class Game {
   }
 
 
-  addClickEvents () {
-    for (let i = 0; i < (GAME_ROWS * this.gameSize); i++) {
-      document.getElementById('card_container' + i).addEventListener('click', this.chooseCard)
-    }
+  // addClickEvents () {
+  //   for (let i = 0; i < (GAME_ROWS * this.gameSize); i++) {
+  //     document.getElementById('card_container' + i).addEventListener('click', this.chooseCard)
+  //   }
+  // }
+
+  addClickEvents (card) {
+    document.getElementById('card_container' + card).addEventListener('click', this.chooseCard)
   }
 
-  removeClickEvents () {
-    for (let i = 0; i < (GAME_ROWS * this.gameSize); i++) {
-      document.getElementById('card_container' + i).removeEventListener('click', this.chooseCard)
-    }
+  removeClickEvents (card) {
+      document.getElementById('card_container' + card).removeEventListener('click', this.chooseCard)
   }
 
-  flipCard (numcontainer) {
-    document.getElementById('card_container' + numcontainer).firstChild.classList.add('hiden')
-    document.getElementById('card_container' + numcontainer).lastChild.classList.remove('hiden')
+  showCard (card) {
+    document.getElementById('card_container' + card).firstChild.classList.add('hiden')
+    document.getElementById('card_container' + card).lastChild.classList.remove('hiden')
+  }
+  hideCard (card) {
+    document.getElementById('card_container' + card).firstChild.classList.remove('hiden')
+    document.getElementById('card_container' + card).lastChild.classList.add('hiden')
   }
 
   chooseCard (ev) {
     switch (game.selected_cards) {
       case 0:
-        // debugger
         this.first_card = ev.target.dataset.position
         this.first_cont = ev.target.dataset.container
-        this.flipCard (this.first_cont)
+        this.showCard (this.first_cont)
         game.selected_cards++
         break
 
         case 1:
         this.second_card = ev.target.dataset.position
         this.second_cont = ev.target.dataset.container
-        this.flipCard (this.second_cont)
+        this.removeClickEvents (this.first_cont)
+        this.showCard (this.second_cont)
         if (this.first_card === this.second_card) {
-          console.log('match')
+          this.removeClickEvents (this.second_cont)
+          this.acierto_seguido++
+          this.puntaje++
+          puntaje.innerHTML = this.puntaje
+          if (this.acierto_seguido == 2) {
+            this.aciertos_seguidos++
+            this.puntaje += 5
+            aciertos.innerHTML = this.aciertos_seguidos
+            puntaje.innerHTML = this.puntaje
+            this.acierto_seguido = 1
+          }
         } else {
-          console.log('intenta nuevamente')
+          this.addClickEvents (this.first_cont)
+          setTimeout (() => {this.hideCard (this.first_cont)}, 1000)
+          setTimeout (() => {this.hideCard (this.second_cont)}, 1000)
+          this.acierto_seguido = 0
+          this.errores++
+          errores.innerHTML = this.errores
         }
           game.selected_cards =0
       break
